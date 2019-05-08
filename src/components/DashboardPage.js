803d660d2f06fe0154ba9b3c00c302723665e665
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2019-05-08 16:39:49 +0200

    fixed bugs

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,76 +1,69 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react';
-import axios from 'axios';
-import { subscribe } from 'mqtt-react';
-
 
 const AnyReactComponent = () => <img className="marker" src="/images/marker.png" />;
 
 export class DashboardPage extends Component {
   state = {
     center: {
-      lat: 55.371210,
+      lat: 55.371208,
       lng: 10.429230,
     },
-    zoom: 16,
+    zoom: 15,
+    lat: undefined,
+    lng: undefined,
     min: undefined,
     max: undefined
   };
 
-  componentWillReceiveProps(){
-    console.log(this.props.location)
-  }
-  
-  bin2String(array) {
-    var result = "";
-    for (var i = 0; i < array.length; i++) {
-      result += String.fromCharCode(parseInt(array[i], 2));
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.location[0] !== undefined) {
+      let obj = String(nextProps.location[nextProps.location.length - 1]);
+      var lat = parseFloat(obj.slice(8, 16));
+      var lng = parseFloat(obj.slice(25, 33));
+      this.setState(prevState => ({ ...prevState,  lat, lng}));
     }
-    return result;
   }
-  
-  messageList = (props) => (
-    <ul>
-      {props.data.map( message => <li>{message}</li> )}
-    </ul>
-  );
 
   onMinChange = (e) => {
     const min = e.target.value
-    this.setState(() => ({min}))
+    this.setState(() => ({ min }))
   };
 
   onMaxChange = (e) => {
     const max = e.target.value
-    this.setState(() => ({max}))
+    this.setState(() => ({ max }))
   };
-  
+
   setRange = () => {
-    console.log('min:',this.state.min,'max:', this.state.max)
+    console.log('min:', this.state.min, 'max:', this.state.max)
   }
 
-  
+  getState = () => {
+    console.log(this.state);
+  }
+
 
   render() {
     return (
       <div>
-      <input onChange={this.onMinChange} type="text" placeholder='Min temperature'/>
-      <input onChange={this.onMaxChange} type="text" placeholder='Max temperature'/>
-      <button onClick = {this.setRange} >Set range</button>
-      <div style={{ height: '75vh', width: '75%' }}>
-      <br/>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyAEkzhcBPPaq0AZ6-Xr-ME8J6ux2eN1Yq0' }}
-        defaultCenter={this.state.center}
-        defaultZoom={this.state.zoom}
-      >
-        <AnyReactComponent
-          lat={this.state.center.lat}
-          lng={this.state.center.lng}
-        />
-      </GoogleMapReact>
-    </div>
-    </div>
+        <input onChange={this.onMinChange} type="text" placeholder='Min temperature' />
+        <input onChange={this.onMaxChange} type="text" placeholder='Max temperature' />
+        <button onClick={this.setRange} >Set range</button>
+        <div style={{ height: '75vh', width: '75%' }}>
+          <br />
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyAEkzhcBPPaq0AZ6-Xr-ME8J6ux2eN1Yq0' }}
+            defaultCenter={this.state.center}
+            defaultZoom={this.state.zoom}
+          >
+            <AnyReactComponent
+              lat={this.state.lat}
+              lng={this.state.lng}
+            />
+          </GoogleMapReact>
+        </div>
+      </div>
     )
   }
 }
--- src/components/MQQT.js
+++ src/components/MQQT.js
@@ -9,19 +9,13 @@ export class PostMessage extends React.Component {
       //MQTT client is passed on
       const { mqtt } = this.props;
       mqtt.publish('cleverCup/location', 'Get location');
+      //console.log(this.props.data)
   }  
-
-  MessageList = (props) => (
-    console.log(props.data)
-  );
    
   render() {
     return (
       <div>
       <button onClick={this.sendMessage.bind(this)}>Get location</button>
-      <ul>
-      {this.props.data.map( message => <li>{message}</li> )}
-    </ul>
     <DashboardPage location={this.props.data} />
       </div>
     );
